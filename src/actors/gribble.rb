be979e73abf3a19a78df6084128f36cbed946f38
John Croisant <john@croisant.net>
2012-06-14 23:30:21 -0500

    Made gribble actors physical.
    Also render a line to show their rotation. Otherwise, the difference
    between a circle rolling and sliding would not be apparent.

--- src/actors/gribble.rb
+++ src/actors/gribble.rb
@@ -1,19 +1,34 @@
 
 define_behavior :gribbly do
   setup do
-    actor.has_attributes( color:  Color::WHITE,
-                          radius: 10 )
+    actor.has_attributes( color:   Color::WHITE,
+                          radius:  10,
+                          density: 20,
+                          elasticity: 0.5,
+                          friction:   0.8 )
+
+    add_behavior( :physical,
+                  shape:  :circle,
+                  radius: actor.radius,
+                  mass:   actor.density * actor.radius**2 * Math::PI,
+                  elasticity: actor.elasticity,
+                  friction:   actor.friction )
   end
 end
 
 
 define_actor :gribble do
-  has_behaviors :positioned, :gribbly
+  has_behaviors :gribbly
   view do
     draw do |target, x_off, y_off, z|
-      target.draw_circle_filled( actor.x + x_off, actor.y + y_off,
-                                 actor.radius, actor.color,
-                                 z )
+      x = actor.x + x_off
+      y = actor.y + y_off
+      
+      target.draw_circle_filled( x, y, actor.radius, actor.color, z )
+
+      angle = actor.body.rot * actor.radius
+      target.draw_line( x, y, x + angle.x, y + angle.y,
+                        Color::BLACK, z )
     end
   end
 end
