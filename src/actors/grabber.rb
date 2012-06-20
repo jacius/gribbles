c21feaeeb291d541d4330b943b820ad93455aac7
John Croisant <john@croisant.net>
2012-06-20 09:09:46 -0400

    Somewhat better grabber behavior (still not satisfactory, though).
    - When grabbing something, grabber moves physically via slew, not warp,
      so the effect on the grabbed actor should be somewhat more stable.
    - Use a pinjoint instead of a damped spring, so the grabbed actor
      doesn't go flying around as much.

--- src/actors/grabber.rb
+++ src/actors/grabber.rb
@@ -1,36 +1,45 @@
 
 define_behavior :grabby do
-  requires :physics_manager, :input_manager
+  requires :physics_manager, :input_manager, :director
   setup do
-    actor.has_attributes( target: nil )
-    reacts_with :grab, :ungrab, :move
+    actor.has_attributes( target: nil,
+                          goal_pos: vec2(0,0) )
+
+    reacts_with :grab, :ungrab, :move_to
     
     i = input_manager
     i.reg :mouse_motion do |event|
       x,y = event[:data]
-      move(x,y)
+      actor.goal_pos = vec2(x,y)
+    end
+
+    director.when :update do |time_ms|
+      update(time_ms)
     end
 
     @constraint = nil
     actor.shape.layers = 2
+
+    actor.body.
+      apply_force(-physics_manager.gravity * actor.body.mass,
+                  CP::ZERO_VEC_2)
   end
 
   helpers do
     def grab(target)
-      return actor.ungrab() if target.nil? 
-      actor.target = target
+      unless target
+        return actor.ungrab()
+      end
 
-      rest_length = 0
-      stiffness = 10000000
-      damping = 20000
+      actor.target = target
+      actor.react_to :warp, target.body.p
       @constraint =
-        CP::Constraint::DampedSpring.new( actor.body, target.body,
-                                          CP::ZERO_VEC_2, CP::ZERO_VEC_2,
-                                          rest_length, stiffness, damping )
+        CP::Constraint::PinJoint.new( actor.body, target.body,
+                                      CP::ZERO_VEC_2, CP::ZERO_VEC_2 )
       actor.constraints << @constraint
-      actor.target.constraints << @constraint
-      actor.target.body.
-        apply_force(-physics_manager.gravity * actor.target.body.mass,
+      target.constraints << @constraint
+      target.body.
+        apply_force(-physics_manager.gravity * target.body.mass,
                     CP::ZERO_VEC_2)
       physics_manager.register_physical_constraint(@constraint)
     end
@@ -46,18 +55,24 @@ define_behavior :grabby do
       actor.target = nil
     end
 
-    def move(x,y)
-      actor.react_to :warp, vec2(x,y)
+    def update(time_ms)
+      if actor.target
+        actor.body.slew(actor.goal_pos, time_ms/1000.0)
+      else
+        actor.react_to :warp, actor.goal_pos
+      end
     end
   end
   
 end
 
+
 define_actor :grabber do
   has_behavior( physical: {
                   shape:  :circle,
                   radius: 1,
-                  fixed:  true,
+                  # fixed:  true,
+                  mass:   1e6,
                   layer:  2 } )
   has_behavior :grabby
   has_behavior :visible
