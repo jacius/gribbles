cd982c0f4eddc0b943e8576856c9a1a4765c37c2
John Croisant <john@croisant.net>
2012-06-20 13:02:50 -0400

    Made gribbles randomly move on their own.

--- src/actors/gribble.rb
+++ src/actors/gribble.rb
@@ -1,11 +1,14 @@
 
 define_behavior :gribbly do
+  requires :director
+  
   setup do
-    actor.has_attributes( color:   Color::WHITE,
-                          radius:  10,
-                          density: 20,
-                          elasticity: 0.5,
-                          friction:   0.8 )
+    actor.has_attributes( color:         Color::WHITE,
+                          radius:        10,
+                          density:       20,
+                          elasticity:    0.5,
+                          friction:      0.8,
+                          jitter_chance: 0.1)
 
     add_behavior( :physical,
                   shape:  :circle,
@@ -16,6 +19,32 @@ define_behavior :gribbly do
 
     actor.shape.layers = 1
     actor.body.v_limit = 2000
+
+    director.when(:update) { |event| update(event) }
+
+    @time_accum = 0
+    reacts_with :jitter
+  end
+
+  helpers do
+    def update(time_ms)
+      @time_accum += time_ms
+      while @time_accum > 1000
+        if rand() < actor.jitter_chance
+          actor.react_to :jitter
+          @time_accum -= 1000
+        else
+          @time_accum -= 100 + rand(50)
+        end
+      end
+    end
+
+    def jitter
+      angle = rand() * -Math::PI
+      mag = actor.radius
+      v = (CP::Vec2.for_angle(angle) + vec2(0, -0.5)) * mag
+      actor.body.apply_impulse(v*actor.body.mass, CP::ZERO_VEC_2)
+    end
   end
 end
 
